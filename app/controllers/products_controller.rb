c06f7ccb7e6a4235abba5323544a5c7ae040601f
CoderRx001 <carrillo964@gmail.com>
2017-05-03 18:24:21 -0500

    working url

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,8 @@
 class ProductsController < ApplicationController
+  
+  def popcorn_page
+    @popcorn = Popcorn.all 
+    render 'popcorn_page.html.erb'
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+product = Popcorn.new(brand: "Fluffy", name: "cheese & pepper", price: "", image: "http://www.101cookbooks.com/mt-static/images/food/turmeric-popcorn-recipe-3.jpg", description: "")
+product.save
+
+product = Popcorn.new(brand: "Fluffy", name: "", price: "", image: "http://www.101cookbooks.com/mt-static/images/food/turmeric-popcorn-recipe-3.jpg", description: "")
+product.save
+
+product = Popcorn.new(brand: "Fluffy", name: "", price: "", image: "http://www.101cookbooks.com/mt-static/images/food/turmeric-popcorn-recipe-3.jpg", description: "")
+product.save
+
+product = Popcorn.new(brand: "Fluffy", name: "", price: "", image: "http://www.101cookbooks.com/mt-static/images/food/turmeric-popcorn-recipe-3.jpg", description: "")
+product.save
+
+product = Popcorn.new(brand: "Fluffy", name: "", price: "", image: "http://www.101cookbooks.com/mt-static/images/food/turmeric-popcorn-recipe-3.jpg", description: "")
+product.save
+
+product = Popcorn.new(brand: "Fluffy", name: "", price: "", image: "http://www.101cookbooks.com/mt-static/images/food/turmeric-popcorn-recipe-3.jpg", description: "")
+product.save
+
+product = Popcorn.new(brand: "Fluffy", name: "", price: "", image: "http://www.101cookbooks.com/mt-static/images/food/turmeric-popcorn-recipe-3.jpg", description: "")
+product.save
+
+product = Popcorn.new(brand: "Fluffy", name: "", price: "", image: "http://www.101cookbooks.com/mt-static/images/food/turmeric-popcorn-recipe-3.jpg", description: "")
+product.save
+
+product = Popcorn.new(brand: "Fluffy", name: "", price: "", image: "http://www.101cookbooks.com/mt-static/images/food/turmeric-popcorn-recipe-3.jpg", description: "")
+product.save
+
+
